61ca258f46fbe44a5e859e5b13b9e5de3842783a
Harm Dermois <hdermois@desktop-21.students.os3.nl>
2015-06-10 11:32:14 +0200

    Updted the different cores scripty

--- code/shell/different_cores.sh
+++ code/shell/different_cores.sh
@@ -63,6 +63,7 @@ for c in $CPU
 do
     for i in $(seq 1 $ITERATIONS)
   do
-    { time prun -v -np 1 -sge-script mpi_host_script $MPI/$EXE $SCALE $EDGEFACTOR $c &> $REPO/$RESULTS/1nodes_"$SCALE"scale_"$EDGEFACTOR"edge_"$OMP"omp_"$c"cpu_"$EXE"$i.txt ; } 2>> $REPO/$RESULTS/1nodes_"$SCALE"scale_"$EDGEFACTOR"edge_"$OMP"omp_"$c"cpu_"$EXE".time 
+      echo prun $RES -v -np 1 -sge-script mpi_host_script $MPI/$EXE $SCALE $EDGEFACTOR $c
+    { time prun $RES -v -np 1 -sge-script mpi_host_script $MPI/$EXE $SCALE $EDGEFACTOR $c &> $REPO/$RESULTS/1nodes_"$SCALE"scale_"$EDGEFACTOR"edge_"$OMP"omp_"$c"cpu_"$EXE"$i.txt ; } 2>> $REPO/$RESULTS/1nodes_"$SCALE"scale_"$EDGEFACTOR"edge_"$OMP"omp_"$c"cpu_"$EXE".time 
   done
 done
